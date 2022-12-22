21b6facd00a34278b0d779c45768e137f3b1a7b3
nmt132 <114457017+nmt132@users.noreply.github.com>
2022-12-22 11:59:36 +0700

    Create инструкции пайтон.py

---
+++ инструкции пайтон.py
@@ -0,0 +1,19 @@
+a = [x for x in range(10)]
+print(a)
+
+генератор
+
+S = "012345671"
+a = list(S)
+print(a)
+e = a.count('1')
+print(e)
+i = a.index(max(a))
+print(i)
+t =list( map(int,a))
+print(t)
+print (t[::-1])
+f = t.sort(reverse = True)
+print(f)
+
+списки
