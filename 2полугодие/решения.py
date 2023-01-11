8369ab8fa0dedbe2b84b102f8ecf0784a73f36a1
nmt132 <114457017+nmt132@users.noreply.github.com>
2023-01-11 10:14:44 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -1,1 +1,31 @@
-def f5():
+def f8():
+  count=0
+  for a in range (1,8):
+      for b in range (8):
+          for c in range (8):
+              for d in range (8):
+                  for e in range (8):
+                      s=str(a)+str(b)+str(c)+str(d)+str(e)
+                      if s.count('6')==1 and s.index('6')==len(s)-1 and int(s[len(s)-2])%2==0:
+                          count+=1
+                      if s.count('6')==1 and s.index('6')==0 and int(s[1])%2==0:
+                          count+=1
+                      if s.count('6')==1 and s.index('6')<len(s)-1 and s.index('6')>0 and int(s[s.index('6')-1])%2==0 and int(s[s.index('6')+1])%2==0:
+                          count+=1
+print(count)
+def f81():
+  from itertools import product
+  nums=product('01234567',repeat=5)
+  k=0
+  n='16 36 56 76 61 63 65 67'
+  nn=n.split()
+  for n in nums:
+      numb=''.join(n)
+      sp=[]
+      if numb.count('6')==1 and numb[0]!='0':
+          for x in nn:
+              if x in numb:
+                  sp.append(x)
+          if not sp: 
+              k+=1
+  print(k)
