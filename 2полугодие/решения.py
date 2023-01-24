9bea2eea92989fbb534b54b4e131c94773b5cc2b
nmt132 <114457017+nmt132@users.noreply.github.com>
2023-01-24 10:00:05 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -29,3 +29,16 @@ def f81():
           if not sp: 
               k+=1
   print(k)
+def f12:
+  for i in range (2,1000):
+    k=0
+    for n in range(2,i-1):
+        if i%n == 0:
+            k=1
+    if k ==0:
+        d= (i-117)%4
+        if d==0:
+            if i>121:
+                f=(i-117)/4
+                print(f)
+
