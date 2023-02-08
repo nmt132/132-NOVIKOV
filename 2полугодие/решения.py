489d2a08a7eae6d93e6b105c98ec01cbf216117b
nmt132 <114457017+nmt132@users.noreply.github.com>
2023-02-08 10:09:09 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -70,3 +70,12 @@ def f23:
   print(f(1,10,10))
   print(f(10,35,25))
 
+def f223:
+  def f(x,y):
+    if x>y or x==17:
+        return 0
+
+    elif x==y: return 1
+    return f(x+1,y) + f(x*2,y)
+  print(f(1,10)*f(10,35))
+
