97ef4b5c16bfd3d5ecc99618a54fb8f125cc05a2
nmt132 <114457017+nmt132@users.noreply.github.com>
2023-02-21 09:54:52 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -95,3 +95,21 @@ def f161:
       elif n>1: return n*f(n-1)
     
   print(f(2023)/f(2020))
+def f17:
+  with open('17.txt') as f:
+    a=[int(x) for x in (f)]
+  count=0
+  for i in range (len (a)-1):
+      if abs (a[i]%10==3 and abs (a[i+1]%10!=3)) or (abs (a[i])%10!=3 and abs(a[i+1])%10==3):
+          count+=1
+  print(count)
+  sp=[]
+  sp2=[]
+  for i in a:
+      if i%10==3:
+          sp.append(i)
+      maxi= max(sp)**2
+      if a[i]**2 +a[i+1]**2>=maxi:
+          count+=1
+          sp2.append()
+      print(count,max(sp2))
