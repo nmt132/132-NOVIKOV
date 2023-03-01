4ac80fdd3bef135aacfd58744ac40c200c0f250b
nmt132 <114457017+nmt132@users.noreply.github.com>
2023-03-01 09:53:44 +0700

    Update f.py

--- 2полугодие/f.py
+++ 2полугодие/f.py
@@ -2,3 +2,8 @@ for i in range(2023,10**10,2023):
     n=str(i)
     if n[0]=='1' and n[2:6]=='2139' and n[-1]=='4':
         print(i,i//2023)
+for i in range(13450608,10**20):
+    n=str(i)
+    if n[0]=='1' and n[1:5]=='2345' and n[8]=='8'and n[6] =='6':
+        if i%17==0:
+            print(i,i//17)
